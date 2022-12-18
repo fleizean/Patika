d8fd62618a9ec7b49af4248cc002358cfcd6c65f
fleizean <nsyagz@gmail.com>
2022-12-18 14:31:38 +0300

    patika.dev/fleizean

---
+++ NetCSharp/Array Sinifi Methodlari/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] sumArray = {23, 12, 5, 1, 3, 99, 55};
+            
+            Console.WriteLine("**** SIRALANMAMIS HALI ****");
+            foreach(var sayi in sumArray)
+                Console.Write(sayi + " ");
+            
+            Console.WriteLine("\n**** SIRALANMIS HALI ****");
+            Array.Sort(sumArray);
+            foreach(var sayi in sumArray)
+                Console.Write(sayi + " ");
+            Console.WriteLine("\n**** ARRAY CLEAR ****");
+            Array.Clear(sumArray, 2, 2);
+            foreach(var sayi in sumArray)
+                Console.Write(sayi + " ");
+            Console.WriteLine("\n**** ARRAY REVERSE ****");
+            Array.Reverse(sumArray);
+            foreach(var sayi in sumArray)
+                Console.Write(sayi + " ");
+            Console.WriteLine("\n**** ARRAY IndexOf ****");
+            Console.WriteLine(Array.IndexOf(sumArray, 23));
+            Console.WriteLine("\n**** ARRAY Resize ****");
+            Array.Resize<int>(ref sumArray, 9);
+            sumArray[8] = 21;
+            foreach(var sayi in sumArray)
+                Console.Write(sayi + " ");
+        }
+    }
+}
