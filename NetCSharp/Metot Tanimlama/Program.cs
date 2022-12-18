a133acbb48ee2bd110c0d5e0c679f5f3af66fd57
fleizean <nsyagz@gmail.com>
2022-12-18 14:38:39 +0300

    patik.dev/fleizean

---
+++ NetCSharp/Metot Tanimlama/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            Console.WriteLine(a + b);
+
+            int sonuc = Topla(a, b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYazdir(Convert.ToString(sonuc));
+
+            int sonuc2 = ornek.ArttirVeTopla(ref a, ref b);
+            ornek.EkranaYazdir(Convert.ToString(sonuc2));
+            ornek.EkranaYazdir(Convert.ToString(a+b));
+        }
+        static int Topla(int s1, int s2)
+        {
+            return (s1 + s2);
+        }
+    }
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public int ArttirVeTopla(ref int s1,ref int s2)
+        {
+            s1++;
+            s2++;
+            return(s1 + s2);
+        }
+    }
+}
+
