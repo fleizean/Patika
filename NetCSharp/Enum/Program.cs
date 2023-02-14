a1250ff02295c6b9c36665f2c20302c645fedc38
Enes Yağız <fleizean@Enes-MacBook-Air.local>
2023-02-14 19:15:04 +0300

    Patika

---
+++ NetCSharp/Enum/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace struct_yapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = 20; // default value
+            Console.WriteLine(Days.Sunday);
+            Console.WriteLine((int)Days.Saturday);
+
+            Console.Write("Enter a Temperature: ");
+            temperature = Convert.ToInt32(Console.ReadLine());
+            
+            if(temperature <= (int)WeatherForecast.Clear)
+                Console.WriteLine("Wait for the weather to warm up to go outside.");
+            else if(temperature >= (int)WeatherForecast.Hot)
+                Console.WriteLine("Wait for the weather to cool before going outside.");
+            else if(temperature >= (int)WeatherForecast.Clear && temperature < (int)WeatherForecast.VeryHot)    
+                Console.WriteLine("Let's go outside.");
+        }
+    }
+
+    enum Days
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday = 23,
+        Saturday,
+        Sunday
+    }
+
+    enum WeatherForecast
+    {
+        Chilly = 20,
+        Clear = 15,
+        Cold = 1,
+        Foggy = 8,
+        Hot = 25,
+        VeryHot = 30,
+        Snowy = -4,
+        Sunny = 23
+    }
+}
---
+++ NetCSharp/Struct_Yapi/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace struct_yapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle._longestSide = 4;
+            rectangle._shortestSide = 3;
+            Console.WriteLine("Class Area Result: {0}",rectangle.CalculateArea());
+
+            Console.WriteLine("**************");
+            
+            Rectangle_Struct s_rectangle = new Rectangle_Struct(3, 4);
+            Console.WriteLine("Struct Area Result: {0}",s_rectangle.CalculateArea());
+
+        }
+    }
+
+    class Rectangle
+    {
+        public int _shortestSide;
+        public int _longestSide;
+
+        public Rectangle()
+        {
+            _shortestSide = 3;
+            _longestSide = 4;
+        }
+
+        public long CalculateArea()
+        {
+            return (this._shortestSide * this._longestSide);
+        }
+    }
+
+    struct Rectangle_Struct
+    {
+        public int shortestSide;
+        public int longestSide;
+
+        public Rectangle_Struct(int shortestside, int longestside)
+        {
+
+            shortestSide = shortestside;
+            longestSide = longestside;
+        }
+
+        public long CalculateArea()
+        {
+            return (this.shortestSide * this.longestSide);
+        }
+    }
+}
