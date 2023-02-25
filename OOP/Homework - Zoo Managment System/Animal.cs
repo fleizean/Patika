c8939c14929a84c7520af40e17dbee3b386324ca
Enes Yağız <fleizean@Enes-MacBook-Air.local>
2023-02-25 18:05:14 +0300

    Patika

--- OOP/Homework - Zoo Managment System/Animal.cs
+++ OOP/Homework - Zoo Managment System/Animal.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace zoo
 {
-    public class Animal
+    public abstract class Animal
     {
         private string _KindType;
         private int _Weight;
@@ -14,8 +14,154 @@ namespace zoo
         public Animal(string kindtype, int weight, int age)
         {
             this._KindType = kindtype;
-            this._Weight = weight;
-            this._Age = age;
+            if(weight < 0)
+                this._Weight = 1;
+            else
+                this._Weight = weight;
+            if(age < 0) 
+                this._Age = 0;
+            else
+                this._Age = age;
+        }
+        public string KindType {
+        get { return _KindType; }
+        set { _KindType = value; }
+        }
+        public int Weight {
+        get { return _Weight; }
+        set { _Weight = value; }
+        }
+        public int Age {
+        get { return _Age; }
+        set { _Age = value; }
+        }
+
+        public abstract double GetDosage();
+        public abstract void GetFeedSchedule();
+    }
+
+    /* Equine */
+    public class Horse : Animal
+    {
+        public Horse(string name, int weight, int age) : base(name, weight, age) {}
+
+        public override double GetDosage() {
+            return (2.1);
+        }
+
+        public override void GetFeedSchedule() {
+            Console.WriteLine("*********** FEED SCHEDULE ***********");
+            Console.WriteLine("Mon: 3");
+            Console.WriteLine("Tue: 2");
+            Console.WriteLine(value: "Wed: 1");
+            Console.WriteLine("Thu: 2");
+            Console.WriteLine("Fri: 3");
+            Console.WriteLine("Sat: 1");
+            Console.WriteLine("Sun: 2");
+            Console.WriteLine("*************************************");
+        }
+    }
+
+    public class Lion : Animal
+    {
+        public Lion(string name, int weight, int age) : base(name, weight, age) {}
+        public override double GetDosage() {
+            return (2.1);
+        }
+
+        public override void GetFeedSchedule() {
+            Console.WriteLine("*********** FEED SCHEDULE ***********");
+            Console.WriteLine("Mon: 3");
+            Console.WriteLine("Tue: 2");
+            Console.WriteLine(value: "Wed: 1");
+            Console.WriteLine("Thu: 2");
+            Console.WriteLine("Fri: 3");
+            Console.WriteLine("Sat: 1");
+            Console.WriteLine("Sun: 2");
+            Console.WriteLine("*************************************");
+        }
+    }
+
+    /* Feline */
+        public class Tiger : Animal
+    {
+        public Tiger(string name, int weight, int age) : base(name, weight, age) {}
+
+        public override double GetDosage() {
+            return (2.1);
+        }
+
+        public override void GetFeedSchedule() {
+            Console.WriteLine("*********** FEED SCHEDULE ***********");
+            Console.WriteLine("Mon: 3");
+            Console.WriteLine("Tue: 2");
+            Console.WriteLine(value: "Wed: 1");
+            Console.WriteLine("Thu: 2");
+            Console.WriteLine("Fri: 3");
+            Console.WriteLine("Sat: 1");
+            Console.WriteLine("Sun: 2");
+            Console.WriteLine("*************************************");
+        }
+    }
+
+    public class Zebra : Animal
+    {
+        public Zebra(string name, int weight, int age) : base(name, weight, age) {}
+        public override double GetDosage() {
+            return (2.1);
+        }
+
+        public override void GetFeedSchedule() {
+            Console.WriteLine("*********** FEED SCHEDULE ***********");
+            Console.WriteLine("Mon: 3");
+            Console.WriteLine("Tue: 2");
+            Console.WriteLine(value: "Wed: 1");
+            Console.WriteLine("Thu: 2");
+            Console.WriteLine("Fri: 3");
+            Console.WriteLine("Sat: 1");
+            Console.WriteLine("Sun: 2");
+            Console.WriteLine("*************************************");
+        }
+    }
+    /* Rodent */
+        public class Beaver : Animal
+    {
+        public Beaver(string name, int weight, int age) : base(name, weight, age) {}
+
+        public override double GetDosage() {
+            return (2.1);
+        }
+
+        public override void GetFeedSchedule() {
+            Console.WriteLine("*********** FEED SCHEDULE ***********");
+            Console.WriteLine("Mon: 3");
+            Console.WriteLine("Tue: 2");
+            Console.WriteLine(value: "Wed: 1");
+            Console.WriteLine("Thu: 2");
+            Console.WriteLine("Fri: 3");
+            Console.WriteLine("Sat: 1");
+            Console.WriteLine("Sun: 2");
+            Console.WriteLine("*************************************");
+        }
+    }
+
+    public class Rat : Animal
+    {
+        public Rat(string name, int weight, int age) : base(name, weight, age) {}
+        public override double GetDosage() {
+            return (2.1);
+        }
+
+        public override void GetFeedSchedule() {
+            Console.WriteLine("*********** FEED SCHEDULE ***********");
+            Console.WriteLine("Mon: 3");
+            Console.WriteLine("Tue: 2");
+            Console.WriteLine(value: "Wed: 1");
+            Console.WriteLine("Thu: 2");
+            Console.WriteLine("Fri: 3");
+            Console.WriteLine("Sat: 1");
+            Console.WriteLine("Sun: 2");
+            Console.WriteLine("*************************************");
         }
     }
 }
--- OOP/Homework - Zoo Managment System/Program.cs
+++ OOP/Homework - Zoo Managment System/Program.cs
@@ -6,7 +6,18 @@ namespace zoo
     {
         public static void Main(string[] args)
         {
+            Animal horse = new Horse("Pinto", 500, 6);
+            Animal lion = new Lion("Simba", 300, 8);
+            Animal rat = new Rat("Remy", 2, 2);
 
+            Console.WriteLine(horse.KindType + " weighs " + horse.Weight + " kg and needs " + horse.GetDosage() + " ml of medicine.");
+            horse.GetFeedSchedule();
+
+            Console.WriteLine(lion.KindType + " weighs " + lion.Weight + " kg and needs " + lion.GetDosage() + " ml of medicine.");
+            lion.GetFeedSchedule();
+
+            Console.WriteLine(rat.KindType + " weighs " + rat.Weight + " kg and needs " + rat.GetDosage() + " ml of medicine.");
+            rat.GetFeedSchedule();
         }
     }
 }
